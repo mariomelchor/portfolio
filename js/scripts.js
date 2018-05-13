f8e24464c28ccc4ce17ffdf24c2b9233d2359623
mariomelchor <mario@mariomelchor.com>
2018-05-12 23:13:36 -0700

    added scripts.js added smooth scroll functionality

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Smooth Scroll
+$(".smooth-scroll").click(function() {
+  if ( location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname  ) {
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+    if (target.length) {
+      var pos = target.offset();
+          pos = pos.top;
+      $("html,body").animate( { scrollTop: pos },  800 ); return false;
+    }
+  }
+});
