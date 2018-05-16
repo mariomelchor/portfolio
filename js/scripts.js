ed41ba8aa8297703833b89646a1de97371865299
mariomelchor <mario@mariomelchor.com>
2018-05-15 20:43:07 -0700

    added portfolio items

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
-// Smooth Scroll
-$(".smooth-scroll").click(function() {
-  if ( location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname  ) {
-    var target = $(this.hash);
-    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-    if (target.length) {
-      var pos = target.offset();
-          pos = pos.top;
-      $("html,body").animate( { scrollTop: pos },  800 ); return false;
+$(document).ready(function() {
+
+  // Smooth Scroll
+  $(".smooth-scroll").click(function() {
+    if ( location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname  ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        var pos = target.offset();
+            pos = pos.top;
+        $("html,body").animate( { scrollTop: pos },  800 ); return false;
+      }
     }
-  }
+  });
+
 });
